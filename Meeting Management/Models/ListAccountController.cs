d7e04d56b99f64b9e88bca87cb9ad056a2097f70
NguyenDaiLam <lam.187pm13948@vanlanguni.vn>
2021-06-08 19:23:42 +0700

    Commit pull request

---
+++ Meeting Management/Models/ListAccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Meeting_Management.Models
+{
+    public class ListAccountController : Controller
+    {
+        // GET: ListAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
